459ed93b444c2d7a8b7c476d31fec7c7e3863470
OlgaSoika <olga.soika2011@gmail.com>
2025-12-10 14:31:26 +0000

    Solution for the part 1

--- 2025/day07/solution.cpp
+++ 2025/day07/solution.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <sstream>
 #include <utility>
+#include <set>
 
 bool parseCommandLine(int argc, char* argv[], std::string &filename) {
     for (int i = 1; i < argc; i++) {
@@ -15,8 +16,8 @@ bool parseCommandLine(int argc, char* argv[], std::string &filename) {
     return false;
 }
 
-std::vector<std::string> readInputFile(const std::string &filename) {
-    std::vector<std::string> data;
+std::vector<std::vector<char>> readInputFile(const std::string &filename) {
+    std::vector<std::vector<char>> data;
     std::ifstream inFile(filename);
     
     if (!inFile) {
@@ -26,12 +27,67 @@ std::vector<std::string> readInputFile(const std::string &filename) {
     std::string line;
     while (std::getline(inFile, line)) {
         if (!line.empty()) {
-            data.push_back(line);
+            std::vector<char> chars(line.begin(), line.end());
+            data.push_back(chars);
         }
     }
-
+    
     inFile.close();
     return data;
+}
+
+static int count{0};
+
+std::set<std::pair<int, int>> beams_positions;
+
+bool start(int row, int col, const std::vector<std::vector<char>> &data) {
+
+    if ((row >= data.size()) || (col < 0) || (col >= data[0].size())) {
+        return false;
+    }
+
+    std::pair<int, int> pos = {row, col};
+
+    if (beams_positions.find(pos) != beams_positions.end()) {
+        return false; // the current beam already exists
+    }
+    
+    // save the beam coordinates in the set;
+    beams_positions.insert({row, col});
+    std::cout << "Beam at position (" << row << ", " << col << ") was set" << std::endl;
+
+    return true;
+}
+
+void process(int row, int col, const std::vector<std::vector<char>> &data) {
+    
+    if ((row >= data.size()) || (col < 0) || (col >= data[0].size())) {
+        return;
+    }
+    
+    std::cout << "Position: (" << row << ", " << col << "), Value: '" << data[row][col] << "'" << std::endl;
+    if (data[row][col] == '.') {
+        // move down
+        process(row+1, col, data);
+        return;
+    }
+
+    if (data[row][col] == '^') {
+        // split beam into left and right
+        bool bSplitted = false;
+        if (start(row, col - 1, data)) {
+            // left beam
+            process(row, col - 1, data);
+            bSplitted = true;
+        }
+        if (start(row, col + 1, data)) {// right beam
+            process(row, col + 1, data);
+            bSplitted = true;
+        }
+        if (bSplitted) {
+            count++;
+        }
+    } 
 }
 
 int main(int argc, char* argv[]) {
@@ -43,11 +99,24 @@ int main(int argc, char* argv[]) {
     }
     
     try {
-        std::vector<std::string> data = readInputFile(filename);
+        std::vector<std::vector<char>> data = readInputFile(filename);
         
         std::cout << "Read " << data.size() << " lines from file." << std::endl;
         
-        // Your solution logic goes here
+        //find symbol S
+
+        size_t j = 0;
+        for(; j< data[0].size(); j++) {
+            if (data[0][j] == 'S') {
+                break;
+            }
+        }
+        
+        // move down the rows and calculate beams
+        start(1, j, data);
+        process(1, j, data);
+
+        std::cout << "Total beams: " << count << std::endl;
         
     } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
